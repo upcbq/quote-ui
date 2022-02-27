11c12bcf6952fdea0e657a31304b1f7ae218c9a9
Landon Bland <landon.bland@panerabread.com>
2022-02-27 02:41:50 -0600

    update verse reference to be properly formatted

--- src/utilities/utilityFunctions.ts
+++ src/utilities/utilityFunctions.ts
@@ -1,5 +1,12 @@
 import { IReference } from '@/api/types';
 
+export function displayBook(book: string) {
+  return book
+    .split('-')
+    .map((s) => `${s[0].toUpperCase()}${s.slice(1).toLowerCase()}`)
+    .join(' ');
+}
+
 export function referenceToString(ref: IReference) {
-  return `${ref.book} ${ref.chapter}:${ref.verse}`;
+  return `${displayBook(ref.book)} ${ref.chapter}:${ref.verse}`;
 }
